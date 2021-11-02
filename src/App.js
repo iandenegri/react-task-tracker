ce590106890c168bc2d95baeb4f95d50eef75a32
Ian Denegri <iandenegri@hotmail.com>
2021-11-02 17:25:45 -0400

    Add intergration with test server. Start footer

--- src/App.js
+++ src/App.js
@@ -12,24 +12,74 @@ function App() {
   const [tasks, setTasks] = useState([])
 
   // Load data from fake DB
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromDB = await fetchTasks();
+      setTasks(tasksFromDB);
+    }
+
+    getTasks();
+
+  }, [])
+
+  // Load tasks from fake db
+  const fetchTasks = async() => {
+    const resp = await fetch('http://localhost:5000/tasks')
+    const data = await resp.json();
+
+    return(data);
+  }
+
+  const fetchTask = async(id) => {
+    const resp = await fetch(
+      `http://localhost:5000/tasks/${id}`)
+    const data = await resp.json();
+
+    return(data);
+  }
 
   // Add a task
-  const addTask = function(task){
-    const id = Math.floor(Math.random() * 100000) + 1;
-    const newTask = {id, ...task};
-    setTasks([...tasks, newTask]);
+  const addTask = async function(task){
+    const resp = await fetch(
+      'http://localhost:5000/tasks', 
+      {
+        method:'POST', 
+        headers: {'Content-type': 'application/json'}, 
+        body: JSON.stringify(task)
+      }
+    )
+
+    const data = await resp.json()
+
+    setTasks([...tasks, data]);
   }
 
   // Delete a task
-  const deleteTask = function (id){
+  const deleteTask = async function (id){
+    await fetch(`http://localhost:5000/tasks/${id}`, {method: 'DELETE'})
+    
     setTasks(tasks.filter((task)=>task.id !== id)) // This never actually deletes a task, it only filters out the tasks with the id we've passed...
   }
 
   // Toggle reminder for task
-  const remindTask = function (id){
+  const remindTask = async function (id){
+    const taskToRemind = await fetchTask(id);
+    const updateTask = { ...taskToRemind, reminder: !taskToRemind.reminder}
+
+    const resp = await fetch(
+      `http://localhost:5000/tasks/${id}`, 
+      {
+        method: 'PUT',
+        headers: {'Content-type': 'application/json'},
+        body: JSON.stringify(updateTask)
+      }
+    )
+
+    const data = await resp.json()
+
     setTasks(
       tasks.map(
-        (task) => task.id === id ? {...task, reminder: !task.reminder} : task
+        (task) => task.id === id ? {...task, reminder: data.reminder} : task
       )
     )
   }
@@ -38,7 +88,7 @@ function App() {
   return (
     // Functions should always return one element
     // With many others nested inside
-    <div className="Container">
+    <div className="container">
       <Header setShowForm={() => setShowForm(!showForm)} showForm={showForm} />
       {showForm && <AddTask addTask={addTask} /> }
       {tasks.length > 0 ? <Tasks tasks={tasks} deleteTask={deleteTask} remindTask={remindTask} /> : 'No tasks to show! Add some tasks!'}
